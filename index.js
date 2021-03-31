778da185335e06e3a0fde2c4b624ed030a42a5cc
anup-paul <anuppaul1845@gmail.com>
2021-03-31 19:41:58 +0600

    done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 7000 
+const port = process.env.PORT || 7000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -24,39 +24,72 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const bookDataCollection = client.db(process.env.DB_NAME).collection("books");
-    
-  console.log(" Successfully connected done")
-  app.post('/addBook',(req, res)=>
-  {
-      const newBookData = req.body;
-      console.log('add new book info',newBookData);
-      bookDataCollection.insertOne(newBookData)
-      .then(result=>
-        {
-            res.send(result)
-        })
-  })
+  const addBookDataCollection = client.db(process.env.DB_NAME).collection("addBooks");
+
+  console.log(" Successfully connected done");
 
 
-  app.get('/books',(req, res)=>
-  {
-      bookDataCollection.find({})
-      .toArray((err,items)=>
-      {
-          res.send(items)
+  app.post('/addBook', (req, res) => {
+    const newBookData = req.body;
+    console.log('add new book info', newBookData);
+    bookDataCollection.insertOne(newBookData)
+      .then(result => {
+        res.send(result)
       })
   })
 
 
-  app.get('/selectedBook/:id', (req, res)=>
-  {
-    bookDataCollection.find({_id:ObjectId(req.params.id)})
-    .toArray((err, item)=>
-    {
-      res.send(item);
-    })
+  app.get('/books', (req, res) => {
+    bookDataCollection.find({})
+      .toArray((err, items) => {
+        res.send(items)
+      })
   })
-  
+
+
+
+  app.get('/selectedBook/:id', (req, res) => {
+    bookDataCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((err, item) => {
+        res.send(item);
+      })
+  })
+
+
+
+ app.post('/addOrder', (req, res)=>
+ {
+   const newOrder = req.body;
+   console.log(newOrder);
+   addBookDataCollection.insertOne(newOrder)  
+   .then(result =>
+    {
+      console.log(result)
+      // res.send(result.insertedCount)
+    })
+ })
+
+
+ app.get('/orders',(req,res)=>
+ {
+  addBookDataCollection.find({email:req.query.email})
+   .toArray((err, results)=>
+   {
+     res.send(results)
+   })
+ })
+
+
+
+  app.delete('/delete/:id', (req,res)=>
+  {
+    bookDataCollection.deleteOne({_id:ObjectId(req.params.id)})
+    .then(result =>
+      {
+        console.log(result);
+      })
+  })
+
 });
 
 
